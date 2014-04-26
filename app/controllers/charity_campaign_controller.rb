6f883f998b8ef9f56977d5b115f1f567458e9ae8
Ventsislav <vntzyv@gmail.com>
2014-04-26 18:51:17 +0300

    Changes to the view page

--- app/controllers/charity_campaign_controller.rb
+++ app/controllers/charity_campaign_controller.rb
@@ -2,4 +2,8 @@ class CharityCampaignController < ApplicationController
   def index
     @charities = CharityCampaign.all
   end
+
+  def show
+    @charity = CharityCampaign.find params[:id]
+  end
 end
