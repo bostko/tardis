f5516990d0f80626637ce023e657dbbb1e180e55
Valentin Aitken <bostko@gmail.com>
2014-04-26 16:19:43 +0300

    Fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,4 +11,3 @@ pic = File.open Rails.root.join 'db', 'charity_photos', 'P3167797.jpg'
 CSV.foreach 'db/charity_campaigns.csv', headers: true do |row|
   CharityCampaign.create title: row['title'], description: row['description'], goal: row['goal'], avatar: pic
 end
-
