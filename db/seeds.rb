526021685fbfdfffa756ec9bf378d90ca6a10a57
Peter Kostov <pekostov@gmail.com>
2014-04-27 09:12:46 +0300

    Added additional seeds for money transactions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -20,12 +21,14 @@ Category.create name: 'Социална политика'
 
 transaction1 = MoneyTransaction.new done_by: "Иван Георгиев", amount: BigDecimal.new("10.5"), date: DateTime.now
 transaction2 = MoneyTransaction.new done_by: "Георги Захариев", amount: BigDecimal.new("110.5"), date: DateTime.now
-transaction3 = MoneyTransaction.new done_by: "Мария", amount: BigDecimal.new("-22.5"), date: DateTime.now
+transaction3 = MoneyTransaction.new done_by: "Мария Пертова", amount: BigDecimal.new("-22.5"), date: DateTime.now
+transaction4 = MoneyTransaction.new done_by: "Филип Йорданов", amount: BigDecimal.new("100.0"), date: DateTime.now
+transaction5 = MoneyTransaction.new done_by: "Мария Петрова", amount: BigDecimal.new("-50"), date: DateTime.now
 
 CSV.foreach 'db/charity_campaigns.csv', headers: true do |row|
   pic = File.open Rails.root.join 'db', 'charity_photos', row['charity_photo']
   charity = CharityCampaign.create title: row['title'], description: row['description'], goal: row['goal'], avatar: pic
   charity.categories << category
-  charity.money_transactions = [transaction1, transaction2, transaction3]
+  charity.money_transactions = [transaction1, transaction2, transaction3, transaction4, transaction5]
   charity.save
 end
