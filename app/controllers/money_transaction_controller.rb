6b64ea8ff50c12eb92c90a4fa56c5b1161c8f720
Peter Kostov <pekostov@gmail.com>
2014-04-27 08:57:45 +0300

    Added money text and fix negative numbers

--- app/controllers/money_transaction_controller.rb
+++ app/controllers/money_transaction_controller.rb
@@ -6,6 +6,7 @@ class MoneyTransactionController < ApplicationController
     charity_campaign = CharityCampaign.find params[:id]
     transactions = charity_campaign.money_transactions
     @transaction_list = TransactionList.new transactions
-    p @transaction_list.revenue
+    p @transaction_list.transactions.first { |transaction| transaction.expnese? }.amount.abs.to_s
+    p "Here"
   end
 end
