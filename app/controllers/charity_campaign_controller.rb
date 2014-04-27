60e524eea479c363c0122834a4dfc289c8ee71eb
Valentin Aitken <bostko@gmail.com>
2014-04-27 15:28:57 +0300

    Charity campaign form

--- app/controllers/charity_campaign_controller.rb
+++ app/controllers/charity_campaign_controller.rb
@@ -13,4 +13,23 @@ class CharityCampaignController < ApplicationController
   def happy
     @happy_stories = CharityCampaign.all.select {|x| x.total_amount >= x.goal}
   end
+
+  def new
+    @charity_campaign = CharityCampaign.new
+  end
+
+  def create
+    charity_campaign = CharityCampaign.new(charity_campaign_params)
+    charity_campaign.save
+
+    puts charity_campaign.errors.full_messages
+
+    redirect_to charity_campaign
+  end
+
+  private
+
+  def charity_campaign_params
+    params.require(:charity_campaign).permit(:title, :description)
+  end
 end
