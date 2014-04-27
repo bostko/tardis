dd7d240d3edf0e525784c37a652b0639699300b3
Hack Nelly <hacknelly@localhost.localdomain>
2014-04-27 13:02:31 +0300

    happy endings, layout project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :load_categories, :happy_endings
+
+  def load_categories
+  	@categories = Category.all
+  end
+
+  def happy_endings
+  	@happy_endings = CharityCampaign.all.select {|x| x.total_amount >= x.goal}
+  end
 end
--- app/controllers/charity_campaign_controller.rb
+++ app/controllers/charity_campaign_controller.rb
@@ -2,13 +2,15 @@ require './app/models/transactions_list.rb'
 
 class CharityCampaignController < ApplicationController
   def index
-    @charities = CharityCampaign.all
-    @categories = Category.all
+    @charities = CharityCampaign.all.select {|x| x.total_amount < x.goal}
   end
 
   def show
     @charity = CharityCampaign.find params[:id]
     @transaction_list = TransactionList.new @charity.money_transactions
-    @categories = Category.all
+  end
+
+  def happy
+    @happy_endings = CharityCampaign.all.select {|x| x.total_amount >= x.goal}
   end
 end
